7343720f66921c56afb55215295950e1203ea2c4
Leandro Pereyra <leandro-pereyra@hotmail.es>
2026-03-06 13:12:27 +0100

    Refactor CSS: Divide styles into modular files by component

--- app/views/fragments/head.php
+++ app/views/fragments/head.php
@@ -1,5 +1,12 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title><?php echo APP_NAME; ?></title>
-<link rel="stylesheet" href="<?php echo APP_URL; ?>public/assets/styles/style.css">
+<link rel="stylesheet" href="<?php echo APP_URL; ?>public/assets/styles/main.css">
+<link rel="stylesheet" href="<?php echo APP_URL; ?>public/assets/styles/header.css">
+<link rel="stylesheet" href="<?php echo APP_URL; ?>public/assets/styles/nav.css">
+<link rel="stylesheet" href="<?php echo APP_URL; ?>public/assets/styles/login.css">
+<link rel="stylesheet" href="<?php echo APP_URL; ?>public/assets/styles/tickets-list.css">
+<link rel="stylesheet" href="<?php echo APP_URL; ?>public/assets/styles/ticket-create.css">
+<link rel="stylesheet" href="<?php echo APP_URL; ?>public/assets/styles/ticket-edit.css">
+<link rel="stylesheet" href="<?php echo APP_URL; ?>public/assets/styles/ticket-details.css">
 <link rel="shortcut icon" href="<?php echo APP_URL; ?>public/assets/images/favicon.ico" type="image/x-icon">
